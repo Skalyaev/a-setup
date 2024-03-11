b999650928758aa0e04857abd439661beca50db1
Skalyaeve <anguinau@student.42.fr>
2024-03-11 15:39:52 +0100

    update

--- Makefile
+++ Makefile
@@ -2,21 +2,6 @@ NAME=setup
 SRC_DIR=srcs
 SRC=$(SRC_DIR)/root/main.sh \
 	$(SRC_DIR)/root/parsing.sh \
-	$(SRC_DIR)/ui/gui.sh \
-	$(SRC_DIR)/ui/terminal.sh \
-	$(SRC_DIR)/ui/ide.sh \
-	$(SRC_DIR)/ui/tools.sh \
-	$(SRC_DIR)/pentest/info_gathering.sh \
-	$(SRC_DIR)/pentest/web_analysis.sh \
-	$(SRC_DIR)/pentest/db_assessment.sh \
-	$(SRC_DIR)/pentest/passw_atk.sh \
-	$(SRC_DIR)/pentest/exploitation.sh \
-	$(SRC_DIR)/pentest/privesc.sh \
-	$(SRC_DIR)/pentest/sniff_spoof.sh \
-	$(SRC_DIR)/pentest/wireless_atk.sh \
-	$(SRC_DIR)/pentest/vuln_analysis.sh \
-	$(SRC_DIR)/pentest/reverse.sh \
-	$(SRC_DIR)/pentest/report.sh \
 	$(SRC_DIR)/root/run.sh
 
 GREEN=\033[0;32m
---
+++ src/parse.sh
@@ -0,0 +1,92 @@
+#
+#
+#================================== PARSING
+if [ "$#" -lt 1 ]; then
+    echo -e "$USAGE"
+    exit 1
+fi
+
+COMMAND="$1"
+if [ "$COMMAND" != 'install' -a "$COMMAND" != 'restore' ]; then
+    echo -e "[$RED ERROR $NC] Unknown command: ${GREEN}$1${NC}"
+    exit 1
+fi
+shift
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+    '-p' | '--path')
+        shift
+        if [ "$#" -eq 0 ]; then
+            echo -e "[$RED ERROR $NC] Missing argument for ${GREEN}--path${NC}."
+            exit 1
+        fi
+        if [ -d "$1" ]; then
+            ROOT="$1"
+        else
+            echo -e "[$RED ERROR $NC] path not found: ${GREEN}$1${NC}"
+            exit 1
+        fi
+        ;;
+    "-t" | "--target")
+        shift
+        TARGETS=()
+        while [ "$#" -gt 0 ]; do
+            if [ "${1:0:1}" = - ]; then
+                break
+            fi
+            TARGETS+=($1)
+            shift
+        done
+        if [ ${#TARGETS[@]} -eq 0 ]; then
+            echo -e "[$RED ERROR $NC] Missing argument for ${GREEN}--target${NC}."
+            exit 1
+        fi
+        ;;
+    -e | --exclude)
+        shift
+        EXCLUDES=()
+        while [ $# -gt 0 ]; do
+            if [ ${1:0:1} = - ]; then
+                break
+            fi
+            EXCLUDES+=($1)
+            shift
+        done
+        if [ ${#EXCLUDES[@]} -eq 0 ]; then
+            echo -e "[$RED ERROR $NC] Missing argument for ${GREEN}--exclude${NC}."
+            exit 1
+        fi
+        ;;
+    -s | --silent)
+        SILENT=1
+        shift
+        ;;
+    '-n' | --ninja)
+        NO_APT=1
+        NO_GIT=1
+        NO_CURL=1
+        shift
+        ;;
+    --no-apt)
+        NO_APT=1
+        shift
+        ;;
+    --no-git)
+        NO_GIT=1
+        shift
+        ;;
+    --no-curl)
+        NO_CURL=1
+        shift
+        ;;
+    --no-backup)
+        NO_BACKUP=1
+        shift
+        ;;
+    *)
+        echo -e "[$RED ERROR $NC] Unknown option: ${GREEN}$1${NC}"
+        exit 1
+        ;;
+    esac
+done
