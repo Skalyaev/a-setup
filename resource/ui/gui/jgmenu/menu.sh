27f1e95d75df32ed3f9dd712b12e716906bac1e2
Skalyaeve <anguinau@student.42.fr>
2024-03-26 22:00:07 +0100

    update

---
+++ resource/ui/gui/jgmenu/menu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+PATH='/bin:/sbin:/usr/bin:/usr/sbin'
+
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+GRAY='\033[0;37m'
+NC='\033[0m'
+
+USAGE="${GRAY}===================usage${NC}
+${YELLOW}$(basename "$0") ${BLUE}<command> ${GREEN}[options]${NC}
+
+<command>:
+    ${BLUE}command${NC}:
+    * description
+
+[options]:
+    ${GREEN}-o, --option${NC}
+    * description"
+#
+#
+#================================== PARSE
+if [ "$#" -lt 1 ]; then
+    echo -e "$USAGE"
+    exit 1
+fi
