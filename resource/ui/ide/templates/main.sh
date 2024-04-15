8b6b27c85dce850465efdf35d94baecb56d008bd
Skalyaeve <anguinau@student.42.fr>
2024-04-15 03:38:17 +0200

    update

--- resource/ui/ide/templates/main.sh
+++ resource/ui/ide/templates/main.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
-PATH='/bin:/sbin:/usr/bin:/usr/sbin'
-
+RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
-RED='\033[0;31m'
 BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
 GRAY='\033[0;37m'
 NC='\033[0m'
 
-USAGE="$GRAY===================USAGE$NC
-$YELLOW$(basename "$0") $BLUE<command> $GREEN[options]$NC
+USAGE="$GRAY===================USAGE
+$YELLOW$(basename "$0") $BLUE<target> $GREEN[options]$NC
 
-<command>:
-\t${BLUE}command$NC:
-\t* description
+$BLUE<target>$NC:
+    $RED*$NC description
 
-[options]:
-\t$GREEN-o, --option$NC
-\t* description"
-#
-#
-#==================================PARSE
-if [ "$#" -lt 1 ]; then
+$GREEN[options]$NC:
+-h, --help
+    $RED*$NC Print this message
+"
+if (( "$#" < 1 )); then
     echo -e "$USAGE"
     exit 1
 fi
